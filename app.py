e2a9d3d37748bd9c9310db0b9fcc394e030f7b89
sornraksandod-sketch <sornraksandod@gmail.com>
2026-05-22 15:48:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ if st.button("full_clean_kie"):
     st.switch_page("pages/cute_full_clean_kie.py")
 elif st.button("simple_clean_cute_kie"):
     st.switch_page("pages/simple_clean_cute_kie.py")
-elif st.button("การวิเคราะห์ข้อมูลเชิงสำรวจ"):
-    st.switch_page("pages/app1_discount_calc.py")    
+elif st.button("simple_clean_kie"):
+    st.switch_page("pages/simple_clean_kie.py")    
 elif st.button("การพยากรณ์ยอดขายแบบง่าย"):
     st.switch_page("pages/app1_discount_calc.py")   
 elif st.button("การพยากรณ์ระยะเวบาการให้บริการขนส่ง"):
