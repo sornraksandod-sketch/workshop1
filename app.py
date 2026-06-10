3cd62d3d225197a87fcd291b2bdd58e1b7c63fe5
sornraksandod-sketch <sornraksandod@gmail.com>
2026-06-10 15:17:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,3 +27,7 @@ elif st.button("Best_classify_model"):
     st.switch_page("pages/classify_redbull_sale.py")  
 elif st.button("Clustering_segment"):
     st.switch_page("pages/clustering_segment.py")  
+elif st.button("association_items"):
+    st.switch_page("pages/association_items.py")  
+elif st.button("association_recommend"):
+    st.switch_page("pages/association_recommend.py")  
